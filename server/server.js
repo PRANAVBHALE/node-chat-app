90727c8299eb8ac721720d3d9ee9653f8d06e8c7
PRANAVBHALE <bhalepranav25@gmail.com>
2017-12-08 19:43:53 +0530

    ADD CONNECT AND DISCONNECT EVENTS

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const publicPath  = path.join(__dirname,'../public')
+var io =socketIO(server)
 
 
 const port=process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app)
+
+var io =socketIO(server)
 
 app.use(express.static(publicPath))
 
@@ -18,7 +24,14 @@ console.log(publicPath);
 //   //   pageTitle:'aboutpage',
 //   // })
 // })
+io.on('connection',(socket)=>{
+  console.log('New user connected');
 
-app.listen(port,()=>{
+  socket.on('disconnect',(server)=>{
+    console.log('Connection loged off');
+  })
+})
+
+server.listen(port,()=>{
   console.log(`server is avaliavle on ${port}`);
 })
